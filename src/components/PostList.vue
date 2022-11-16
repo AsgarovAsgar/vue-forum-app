330fd035651219c914e58786f5ffb82765e4002b
Asgarov Asgar <asgara@maestro.st>
2022-11-16 20:36:35 +0400

    refactored everything

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -4,7 +4,7 @@
       <div class="flex flex-col gap-4 w-24">
         <h2 class="text-green-500 text-sm">{{ userById(post.userId).name }}</h2>
         <img class="h-20 w-20 border rounded-full flex-shrink-0 object-cover" :src="userById(post.userId).avatar" alt="">
-        <p class="text-xs">{{ Object.keys(userById(post.userId).posts).length }} posts</p>
+        <p class="text-xs">{{ userById(post.userId).postsCount }} posts</p>
       </div>
       <div class="flex flex-col justify-between w-full">
         <p>{{ post.text }}</p>
@@ -31,12 +31,13 @@ export default {
   },
   methods: {
     userById(userId) {
-      const _users = Object.entries(this.users)
-      for (let user of _users) {
-        if(user[0] !== userId) continue
-        return user[1]
-        // console.log('User: ', user);
-      }
+      // const _users = Object.entries(this.users)
+      // for (let user of _users) {
+      //   if(user[0] !== userId) continue
+      //   return user[1]
+      //   // console.log('User: ', user);
+      // }
+      return this.users.find(u=> u.id === userId)
     }
   }
 }
--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -4,12 +4,12 @@
   <div v-for="thread in threads" :key="thread.id" class=" py-2 px-4 w-full flex justify-between odd:bg-white even:bg-slate-100">
     <div class="w-3/4">
       <p class="text-green-500 text-lg">
-        <router-link :to="{name: 'ThreadShow', params: {id: threadId(thread)}}">{{thread.title}}</router-link>
+        <router-link :to="{name: 'ThreadShow', params: {id: thread.id}}">{{thread.title}}</router-link>
       </p>
       <p class="text-sm">By {{ userById(thread.userId).name }}, {{thread.publishedAt}}</p>
     </div>
     <div class="text-lg w-1/4 flex items-center">
-      <p>{{Object.keys(thread.posts).length}} replies</p>
+      <p>{{thread.posts.length}} replies</p>
     </div>
     <div class="flex gap-2 w-1/4">
       <img class="w-10 h-10 rounded-full object-cover" :src="userById(thread.userId).avatar" alt="">
@@ -41,25 +41,27 @@ export default {
     }
   },
   methods: {
-    threadId(thread) {
-      return Object.entries(thread)[Object.entries(thread).length - 1][1]
-    },
+    // threadId(thread) {
+    //   return Object.entries(thread)[Object.entries(thread).length - 1][1]
+    // },
     postById(postId) {
-      const _posts = Object.entries(this.posts)
-      for (let post of _posts) {
-        if(post[0] !== postId) continue
-        return post[1]
-        // console.log(post);
+      // const _posts = Object.entries(this.posts)
+      // for (let post of _posts) {
+      //   if(post[0] !== postId) continue
+      //   return post[1]
+      //   // console.log(post);
 
-      }
+      // }
+      return this.posts.find(p => p.id === postId)
     },
     userById(userId) {
-      const _users = Object.entries(this.users)
-      for (let user of _users) {
-        if(user[0] !== userId) continue
-        return user[1]
-        // console.log('User: ', user);
-      }
+      // const _users = Object.entries(this.users)
+      // for (let user of _users) {
+      //   if(user[0] !== userId) continue
+      //   return user[1]
+      //   // console.log('User: ', user);
+      // }
+      return this.users.find(u=> u.id === userId)
     }
   }
 }
