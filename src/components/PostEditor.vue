b88aaa6d55589f923e8eff1ad0a479897aef02ef
Asgarov Asgar <asgara@maestro.st>
2022-11-17 23:44:08 +0400

    added base components

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div id="form">
+      <form @submit.prevent="save" class="flex flex-col space-y-4">
+        <!-- <textarea :value="newPostText" @input="newPostText = $event.target.value"></textarea> -->
+        <textarea v-model="text" class="rounded border p-4" cols="30" rows="10"></textarea>
+        <div class="flex justify-end">
+          <button class="rounded px-6 py-3 bg-blue-700 text-white">Submit post</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    save() {
+      const postId = 'aaaa' + Math.random() * 5
+      const post = {
+        id: postId,
+        text: this.text,
+        publishedAt: Math.floor(Date.now() / 1000),
+        userId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1'
+      }
+
+      this.$emit('save', { post })
+      this.text = ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "./main.css";
 import router from '@/router'
+import AppDate from '@/components/AppDate'
 
 const forumApp = createApp(App)
+forumApp.component('AppDate', AppDate)
 forumApp.use(router);
 forumApp.mount("#app");
 
