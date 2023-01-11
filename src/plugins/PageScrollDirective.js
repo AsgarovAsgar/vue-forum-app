828ba73db4109e1d044fe2da34fc42876ebd1dc0
Asgarov Asgar <asgara@maestro.st>
2023-01-11 13:14:13 +0400

    created custom vue directives, installed lodash

---
+++ src/plugins/PageScrollDirective.js
@@ -0,0 +1,17 @@
+import debounce from 'lodash/debounce'
+
+const PageScrollDirective = {
+  mounted(el, binding) {
+    el.__PageScroll__ = debounce(() => {
+      console.log('scroll');
+      binding.value()
+    }, 200, {leading: true})
+    document.addEventListener('scroll', el.__PageScroll__)
+  },
+  unmounted(el, binding) {
+    document.addEventListener('scroll', el.__PageScroll__)
+  },
+}
+export default(app) => {
+  app.directive('page-scroll', PageScrollDirective)
+}
